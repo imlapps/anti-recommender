96b58e63525813db4ddcc1d7a8ce637b629ff3d5
diaHelicoprion00 <reedishigamiwayne00@gmail.com>
2024-01-25 13:56:03 -0500

    test(main): added tests for main

---
+++ backend/app/tests/test_main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi.testclient import TestClient
+
+@asynccontextmanager 
+async def lifespan(app: FastAPI):
+    """
+    A lifespan event to test the storage of wikipedia data in the app state.
+    """
+
+    app.state.wikipedia_storage =  {
+        "test-article" : 
+        {
+        "info": "test-wikipedia-article"
+        }
+    }
+
+    yield 
+
+    app.state.wikipedia_storage = None 
+
+# intialize the FastAPI app and pass in the lifespan event.
+app = FastAPI(lifespan = lifespan)
+
+@app.get("/")
+async def get_wikipedia_storage():
+    """
+    A path operation function of the root endpoint.
+    It returns wikipedia_storage, which was stored in the app state.  
+    """
+    
+    return app.state.wikipedia_storage 
+
+# Test that the root endpoint returns a value stored in the app's state.
+def test_read_main():
+
+    """
+    Test to check that wikipedia_storage is correctly stored in the app state and can be accessed on startup.
+    """
+
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {
+        "test-article" : 
+        {
+        "info": "test-wikipedia-article"
+        }
+    }
+    
