c7020e0058f58b68c85ff34c5dfbbe29d188ec65
diaHelicoprion00 <reedishigamiwayne00@gmail.com>
2024-02-22 12:58:25 -0500

    refactor: improved code and project structure of record

--- backend/app/data/wikipedia_output_path.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-__all__ = ["wikipedia_output_path"]
-
-# The file path for the Wikipedia abstracts output data.
-wikipedia_output_path = Path(__file__).parent / "mini-wikipedia.output.txt"
