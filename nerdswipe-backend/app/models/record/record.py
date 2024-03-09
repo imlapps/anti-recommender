83c6a8e6cec74cd42ce6cc10dde1ed1f50c1826b
diaHelicoprion00 <reedishigamiwayne00@gmail.com>
2024-03-09 13:47:15 -0500

    refactor: converted Record properties into top-level models

--- nerdswipe-backend/app/models/record/record.py
+++ nerdswipe-backend/app/models/record/record.py
@@ -1,22 +1,31 @@
 from pydantic import BaseModel
 
 
+class AbstractInfo(BaseModel):
+    """Pydantic Model that holds the Abstract Information of a Wikipedia Article."""
+
+    title: str = ""
+    abstract: str = ""
+    url: str = ""
+
+
+class Categories(BaseModel):
+    """Pydantic Model to hold a category of a Wikipedia Article."""
+
+    text: str = ""
+    link: str = ""
+
+
+class ExternalLink(BaseModel):
+    """Pydantic Model to hold an external link of a Wikipedia Article."""
+
+    title: str = ""
+    link: str = ""
+
+
 class Record(BaseModel):
-    """Pydantic Model to hold a Wikipedia record."""
+    """Pydantic Model to hold a record."""
 
-    class AbstractInfo(BaseModel):
-        title: str = ""
-        abstract: str = ""
-        url: str = ""
-
-    class Categories(BaseModel):
-        text: str = ""
-        link: str = ""
-
-    class ExternalLink(BaseModel):
-        title: str = ""
-        link: str = ""
-
-    abstract_info: AbstractInfo = AbstractInfo()
+    abstract_info: AbstractInfo | None = None
     categories: tuple[Categories, ...] | None = None
     externallinks: tuple[ExternalLink, ...] | None = None
