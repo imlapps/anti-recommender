e9cdd7bb844112743d717c7b8c4d56c0a53e3973
diaHelicoprion00 <reedishigamiwayne00@gmail.com>
2024-04-10 21:49:18 -0400

    test(backend): added skip decorators to NormalOpenAiAntiRecommender tests

--- backend/app/tests/anti_recommenders/open_ai/test_normal_open_ai_anti_recommender.py
+++ backend/app/tests/anti_recommenders/open_ai/test_normal_open_ai_anti_recommender.py
@@ -1,3 +1,4 @@
+import pytest
 from langchain.schema.runnable import RunnableSequence, RunnableSerializable
 from pytest_mock import MockFixture
 
@@ -5,9 +6,11 @@ from app.anti_recommenders.open_ai.normal_open_ai_anti_recommender import (
     NormalOpenAiAntiRecommender,
 )
 from app.models.anti_recommendation import AntiRecommendation
+from app.models.settings import settings
 from app.models.types.record_type import RecordType
 
 
+@pytest.mark.skipif(settings.ci, reason="don't have OpenAI key in CI")
 def test_build_chain(
     open_ai_normal_anti_recommender: NormalOpenAiAntiRecommender,
 ) -> None:
@@ -19,23 +22,26 @@ def test_build_chain(
     )
 
 
+@pytest.mark.skipif(settings.ci, reason="don't have OpenAI key in CI")
 def test_create_query(
-    open_ai_normal_anti_recommender: NormalOpenAiAntiRecommender, record_key: str,    record_type: RecordType
+    open_ai_normal_anti_recommender: NormalOpenAiAntiRecommender,
+    record_key: str,
+    record_type: RecordType,
 ) -> None:
     """Test that OpenAiNormalAntiRecommender._create_query() returns an OpenAI query when passed a record key."""
 
     assert record_key in open_ai_normal_anti_recommender._create_query(  # noqa: SLF001
-        record_key,
-    record_type
+        record_key, record_type
     )
 
 
+@pytest.mark.skipif(settings.ci, reason="don't have OpenAI key in CI")
 def test_generate_llm_response(
     session_mocker: MockFixture,
     open_ai_normal_anti_recommender: NormalOpenAiAntiRecommender,
     model_response: str,
     record_key: str,
-    record_type: RecordType
+    record_type: RecordType,
 ) -> None:
     """Test that OpenAiNormalAntiRecommender._generate_llm_response() returns a response from an OpenAI large language model."""
 
@@ -48,13 +54,16 @@ def test_generate_llm_response(
 
     assert (
         open_ai_normal_anti_recommender._generate_llm_response(  # noqa: SLF001
-            open_ai_normal_anti_recommender._create_query(record_key, record_type),  # noqa: SLF001
+            open_ai_normal_anti_recommender._create_query(  # noqa: SLF001
+                record_key, record_type
+            ),
             open_ai_normal_anti_recommender._build_chain(),  # noqa: SLF001
         )
         == model_response
     )
 
 
+@pytest.mark.skipif(settings.ci, reason="don't have OpenAI key in CI")
 def test_parse_llm_response(
     open_ai_normal_anti_recommender: NormalOpenAiAntiRecommender,
     anti_recommendations: tuple[AntiRecommendation, ...],
@@ -72,7 +81,8 @@ def test_parse_llm_response(
     assert tuple(anti_recommendation_records) == anti_recommendations
 
 
-def test_generate_anti_recommendations(
+@pytest.mark.skipif(settings.ci, reason="don't have OpenAI key in CI")
+def test_generate_anti_recommendations(  # noqa: PLR0913
     session_mocker: MockFixture,
     open_ai_normal_anti_recommender: NormalOpenAiAntiRecommender,
     record_key: str,
@@ -91,6 +101,7 @@ def test_generate_anti_recommendations(
         anti_recommendations[0].title
         == next(
             open_ai_normal_anti_recommender.generate_anti_recommendations(
-                record_key, record_type)
+                record_key, record_type
+            )
         ).title
     )
