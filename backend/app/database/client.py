3b080062a1fbe542dec5fa9378d90df42341e8f2
diahelicoprion00 <reedishigamiwayne00@gmail.com>
2024-10-09 12:45:44 -0400

    refactor: added isinstance checks to DatabaseClient

--- backend/app/database/client.py
+++ backend/app/database/client.py
@@ -1,13 +1,13 @@
 from typing import cast
-
+import supabase
 from postgrest import APIResponse, SyncFilterRequestBuilder, SyncSelectRequestBuilder
-from supabase import Client, create_client
+from supabase import Client
 
 from app.models import settings
 
 
 class DatabaseClient:
-    """A Database client class."""
+    """A database client class."""
 
     def __init__(self) -> None:
         self.__database_client: Client | None = self.__select_database_client()
@@ -16,11 +16,11 @@ class DatabaseClient:
         """
         Create and return a database client.
 
-        A Supabase client is instantiated if the required parameters are in `settings`.
+        A Supabase `Client` instance is returned if the required parameters are in `settings`.
         """
 
         if settings.supabase_url and settings.supabase_key:
-            return create_client(settings.supabase_url, settings.supabase_key)
+            return supabase.create_client(settings.supabase_url, settings.supabase_key)
 
         return None
 
@@ -31,7 +31,7 @@ class DatabaseClient:
         An `equal_to` filter is added to the query via the `eq` parameter.
         """
 
-        if self.__database_client:
+        if isinstance(self.__database_client, Client):
             return cast(
                 SyncFilterRequestBuilder,
                 self.__database_client.table(table_name).delete().eq(**eq),
@@ -45,7 +45,7 @@ class DatabaseClient:
         An `equal_to` filter is added to the query via the `eq` parameter.
         """
 
-        if self.__database_client:
+        if isinstance(self.__database_client, Client):
             return cast(
                 SyncSelectRequestBuilder,
                 self.__database_client.table(table_name).select(query).eq(**eq),
@@ -55,7 +55,7 @@ class DatabaseClient:
     def insert(self, *, table_name: str, query: dict | list) -> APIResponse | None:
         """Run a `INSERT` query on a table with the name `table_name`."""
 
-        if self.__database_client:
+        if isinstance(self.__database_client, Client):
             return self.__database_client.table(table_name).insert(query).execute()
         return None
 
@@ -66,7 +66,7 @@ class DatabaseClient:
         An `equal_to` filter is added to the query via the `eq` parameter.
         """
 
-        if self.__database_client:
+        if isinstance(self.__database_client, Client):
             return cast(
                 SyncFilterRequestBuilder,
                 self.__database_client.table(table_name).update(query).eq(**eq),
@@ -84,7 +84,7 @@ class DatabaseClient:
         An `INSERT` query is run otherwise.
         """
 
-        if self.__database_client:
+        if isinstance(self.__database_client, Client):
             return (
                 self.__database_client.table(table_name)
                 .upsert(query, on_conflict=constraint)
