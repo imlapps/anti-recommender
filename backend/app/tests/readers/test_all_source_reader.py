892adc87341436ef0e0868c3ccfa23232a0f3fc6
diahelicoprion00 <reedishigamiwayne00@gmail.com>
2024-10-25 14:31:30 -0400

    fix(ci): removed output_file_names from .env.local

--- backend/app/tests/readers/test_all_source_reader.py
+++ backend/app/tests/readers/test_all_source_reader.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+
 from app.models import Record
 from app.readers import AllSourceReader
 
 
-def test_read(all_source_reader: AllSourceReader) -> None:
+def test_read(
+    all_source_reader: AllSourceReader,
+    wikipedia_output_file_path: Path,  # noqa: ARG001
+) -> None:
     """Test that AllSourceReader.read yields the expected output type."""
 
     assert isinstance(next(iter(all_source_reader.read())), Record)
--- backend/app/tests/readers/test_create_readers.py
+++ backend/app/tests/readers/test_create_readers.py
@@ -1,9 +1,14 @@
+from pathlib import Path
+
 from app.models.settings import Settings
 from app.readers import create_readers
 from app.readers.reader import Reader
 
 
-def test_create_readers(settings: Settings) -> None:
+def test_create_readers(
+    settings: Settings,
+    wikipedia_output_file_path: Path,  # noqa: ARG001
+) -> None:
     """Test that create_readers returns the expected tuple of Readers."""
 
     assert isinstance(create_readers(settings)[0], Reader)
